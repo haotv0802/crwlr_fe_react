99561483a395f607f21cd0ea1ad04730dff7ea60
Hao Ho <haotv0802@gmail.com>
2017-11-07 15:40:34 +0700

    Initial commit.

---
+++ src/components/common/Header.js
@@ -0,0 +1,18 @@
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+import LoadingDots from './LoadingDots';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/courses" activeClassName="active">Courses</Link>
+      {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
+      <LoadingDots interval={100} dots={10}/>
+    </nav>
+  );
+};
+
+export default Header;
