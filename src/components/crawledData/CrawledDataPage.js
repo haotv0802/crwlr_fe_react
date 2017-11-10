fd8a2ba2865a887d2f06fe177009438ccacd7e31
Hao Ho <haotv0802@gmail.com>
2017-11-10 13:48:23 +0700

    Move chartData outside of Chart.js class

--- src/components/crawledData/CrawledDataPage.js
+++ src/components/crawledData/CrawledDataPage.js
@@ -9,7 +9,8 @@ class CrawledDataPage extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      recrawlOpen: false
+      recrawlOpen: false,
+      chartData: {}
     };
     this.recrawlData = this.recrawlData.bind(this);
     this.onChange = this.onChange.bind(this);
@@ -64,7 +65,7 @@ class CrawledDataPage extends React.Component {
           )}
           </tbody>
         </table>
-        <Chart/>
+        <Chart chartData={this.state.chartData} legendPosition='bottom'/>
       </div>
     );
   }
@@ -83,6 +84,39 @@ class CrawledDataPage extends React.Component {
       recrawlOpen: !prevState.recrawlOpen
     }));
     console.log(this.state.recrawlOpen);
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: 'Population',
+          data: [12, -19, 3, -5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    });
   }
 }
 
