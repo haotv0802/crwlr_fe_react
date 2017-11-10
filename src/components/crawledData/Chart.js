6dba057ba42e03703bd7a065c2d9d3bf24f2e392
Hao Ho <haotv0802@gmail.com>
2017-11-10 13:32:47 +0700

    Put chartData to state.

--- src/components/crawledData/Chart.js
+++ src/components/crawledData/Chart.js
@@ -4,6 +4,32 @@ import {Bar, Line, Pie} from 'react-chartjs-2';
 class Chart extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      chartData: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, -5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    };
   }
 
   render() {
@@ -11,7 +37,7 @@ class Chart extends Component {
     return(
       <div className="chart">
         <Bar
-          data={this.props.chartData}
+          data={this.state.chartData}
           width={100}
           height={50}
           options={{
@@ -36,39 +62,14 @@ Chart.propTypes = {
   displayTitle: PropTypes.bool.isRequired,
   displayText: PropTypes.string.isRequired,
   displayLegend: PropTypes.bool.isRequired,
-  legendPosition: PropTypes.string.isRequired,
-  chartData: PropTypes.object.isRequired
+  legendPosition: PropTypes.string.isRequired
 };
 
 Chart.defaultProps = {
   displayTitle: true,
   displayText: 'Title HAHAHA',
   displayLegend: true,
-  legendPosition: 'right',
-  chartData: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, -5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  }
+  legendPosition: 'right'
 };
 
 export default Chart;
